12372ceb71501731f7d4bed048d7f7adedd1baad
arihantjain6 <arihantjainog@gmail.com>
2025-08-14 21:24:38 +0530

    first commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react'
+const Login = () => {
+
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log("email is",email)
+    console.log("password is",password)
+
+    setEmail("")
+    setPassword("")
+  }
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <div className='border-2 rounded-xl border-emerald-600 p-20'>
+        <form
+        onSubmit={(e)=>{
+          submitHandler(e);
+        }}
+        className='flex flex-col items-center justify-center'> 
+            <input
+            value={email}
+            onChange={(e)=>
+             setEmail(e.target.value)
+            }
+            required
+            className='border-2 outline-none bg-transparent text-xl border-emerald-600 font-medium py-2 px-6 rounded-full placeholder:text-grey-400' type="email" placeholder='Enter your email' />
+            <input 
+            value={password}
+            onChange={(e)=>{
+              setPassword(e.target.value)
+            }}
+            required 
+            className='border-2 mt-3 outline-none bg-transparent text-xl border-emerald-600 font-medium py-2 px-6 rounded-full placeholder:text-grey-400' type="password" placeholder='Enter password' />
+            <button className='text-white mt-7  bg-emerald-600 hover:bg-emerald-700 font-semibold w-full border-none outline-none text-xl border-emerald-600 py-2 px-8 rounded-full placeholder:text-white' >Log in</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Header from "../others/Header";
+
+const AdminDashboard = () => {
+  return (
+    <div className="w-full h-screen p-7">
+      <Header />
+
+      <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
+        <form className="flex flex-wrap items-start w-full justify-between">
+          <div className="w-1/2">
+            <div>
+              <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
+              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4  " type="text" placeholder="Make a UI design" />
+            </div>
+
+            <div>
+              <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
+              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4  " type="date" /> 
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
+              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4  " type="text" placeholder="employee name" />
+            </div>
+            <div>
+              <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
+              <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4  " type="text" placeholder="design, dev, etc" />
+            </div>
+          </div>
+
+          <div className="2-2/5 flex-col item-start">
+            <h3 className="text-sm text-gray-300 mb-0.5">Discription</h3>
+            <textarea className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4  "  name="" id="" cols="30" rows="10"></textarea>
+            <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">Create Task</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
